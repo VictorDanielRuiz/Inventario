20ff7620ab1a3001f2b1cb943e390760754f42bc
VictorDanielRuiz <victor.ruiz@kener.com.mx>
2024-04-03 10:33:58 -0600

    Actualización de archivos

--- index.js
+++ index.js
@@ -1,13 +1,49 @@
 
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const app = require();
+const cors = require("cors");
+const pool = require("./db");
 
-var app = express();
+//middleware
+app.use(cors);
+app.use(express.json()); //req.body
 
-app.use(express.static(path.join(__dirname, 'front')));
+//ROUTES//
 
-app.get('/login', function(req, res) {
-    res.sendFile('front/login.html', {root: __dirname })
+//create a todo
+
+app.post("/todos", async (req, res) => {
+    try {
+        const { description } = req.body;
+        const newTodo = await pool.query("INSERT INTO todo (description) VALUES($1) RETURNING *",
+            [description]
+        );
+
+        res.json(newTodo.rows[0]);
+        } catch (err) {
+            console.error(err.message);
+        }
 });
 
-app.listen(3000);
+//get all todos
+
+app.get("/todos", async(req, res) => {
+    try {
+        const allTodos = await pool.query("SELECT * FROM todos");
+        res.json(allTodos.rows);
+    } catch (err) {
+        console.error(err.message);
+        
+    }
+})
+
+//get a todo
+
+//update a todo
+
+//delete a todo
+
+app.listen(5000, () => {
+    console.log("El server ha iniciado en el puerto 5000");
+});
+
